29bb363850db089eb45eac15f1d13390b1a0d5e4
Bakhodir <tba-93@yandex.com>
2021-08-18 14:44:30 +0500

    CRUD Universitiy

--- src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
+++ src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.appjparelationships.entity.Student;
+import uz.pdp.appjparelationships.payload.StudentDto;
 import uz.pdp.appjparelationships.repository.StudentRepository;
 
 @RestController
@@ -42,7 +43,37 @@ public class StudentController {
     }
 
     //3. FACULTY DEKANAT
-    //4. GROUP OWNER
+    @GetMapping("/forFaculty/{faculty_id}")
+    public Page<Student> getStudentListForFaculty(@PathVariable Integer faculty_id, @PathVariable int page){
+        Pageable pageable = PageRequest.of(page,10);
+        Page<Student> studentPage = studentRepository.findAllByGroup_FacultyId(faculty_id, pageable);
+        return studentPage;
+    }
 
+    //4. GROUP OWNER
+    @GetMapping("/forGroup/{group_id}")
+    public Page<Student> getStudentListForGroup(@PathVariable Integer group_id, @RequestParam int page){
+        Pageable pageable = PageRequest.of(page,10);
+        Page<Student> studentPage = studentRepository.findAllByGroupId(group_id, pageable);
+        return studentPage;
+    }
+
+    @PostMapping
+    public String addNewStudent(@RequestBody StudentDto studentDto){
+
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Integer id){
+        if (!studentRepository.existsById(id))
+            return "Student not found";
+        studentRepository.deleteById(id);
+        return "Student is deleted";
+    }
+
+    @PutMapping("/{id}")
+    public String editStudent(@PathVariable Integer id, @RequestBody StudentDto studentDto){
+
+    }
 
 }
